22a0d63405ac3169a49a6d878936b2d94d3dc676
Filip Josimovski <fico_josimovski@hotmail.com>
2014-10-22 13:37:43 +0200

    Fix for registrations_controller added additional parametars

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :coy, :title, :email, :password, :password_confirmation
-    devise_parameter_sanitizer.for(:account_update) << :coy, :title, :email, :password, :password_confirmation, :current_password
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:coy, :title, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:coy, :title, :email, :password, :password_confirmation, :current_password) }
   end
 end
