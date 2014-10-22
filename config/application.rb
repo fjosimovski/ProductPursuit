6abdf4eb7c89020682301ad966e54974c6c13137
Filip Josimovski <fico_josimovski@hotmail.com>
2014-10-22 16:07:49 +0200

    Changed icons name in style.css; Added application.css; removed precompiling options in application.rb and production.rb

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Producthunt
   class Application < Rails::Application
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
