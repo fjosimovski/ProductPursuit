4e92d78878c8e0cc17f0e2341e853a2aa331a728
Filip Josimovski <fico_josimovski@hotmail.com>
2014-10-23 13:18:06 +0200

    Added model for Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020094332) do
+ActiveRecord::Schema.define(version: 20141023110745) do
+
+  create_table "posts", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
