ce6627736f2adc5a7f51b6ae00e03f231760e339
Filip Josimovski <fico_josimovski@hotmail.com>
2014-10-28 16:48:08 +0100

    Removed comments from post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,15 @@
 class PostsController < ApplicationController
 before_action :signed_in_user, only: [:create, :destroy]
 
-
   def index
     
   end
 
   def create
-    # render text: "#{params[:post]}"
     @post = current_user.posts.build(post_params)
-    # @post.user_id = current_user.id
-    # @posts = Post.new(post_params)
 
     if @post.save
       flash[:success] = "Post was created"
-      # render text: "#{params[:post]}"
       redirect_to root_url
     else
       render root_url
